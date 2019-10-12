b0a063530aa745e946b8527a5e55928a6b836856
HnakadouriN <hasebenaoki0908@docomo.ne.jp>
2019-10-12 18:46:44 +0900

    シーンの同期

---
+++ tkjp/Assets/Scripts/CubeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class CubeController : MonoBehaviourPunCallbacks
+{
+
+//
+//    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+//        rb = this.GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (photonView.IsMine)
+        {
+            float dx = Input.GetAxis("Horizontal");
+            float dy = Input.GetAxis("Vertical");
+            transform.Translate(dx, dy, 0f);
+        }
+    }
+}
