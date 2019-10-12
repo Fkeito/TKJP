7bca2e5d43e34a8e3bcf1ffca1d51ebb9036acd2
HnakadouriN <hasebenaoki0908@docomo.ne.jp>
2019-10-12 19:37:27 +0900

    use RPC

--- tkjp/Assets/Scripts/Sabaiteikuuuu.cs
+++ tkjp/Assets/Scripts/Sabaiteikuuuu.cs
@@ -4,21 +4,29 @@ using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
 
-public class Sabaiteikuuuu : MonoBehaviourPunCallbacks {
+public class Sabaiteikuuuu : MonoBehaviourPunCallbacks
+{
+    private PhotonView _photonView;
     void Start () {
         PhotonNetwork.ConnectUsingSettings ();
+        _photonView = GetComponent<PhotonView>();
     }
     public override void OnConnectedToMaster () {
         Debug.Log ("接続成功");
-        PhotonNetwork.JoinOrCreateRoom ("ハヘベ", new RoomOptions (), TypedLobby.Default);
+        PhotonNetwork.JoinOrCreateRoom("ハヘベ", new RoomOptions(), TypedLobby.Default);
     }
     public override void OnJoinedRoom () {
         Debug.Log ("マッチング成功");
         if (PhotonNetwork.IsMasterClient) {
+            _photonView.RPC("CreateCube", RpcTarget.AllBuffered);
             Debug.Log ("Master");
         } else {
             Debug.Log ("Guest");
         }
+    }
+
+    private void CreateCube()
+    {
         var v = new Vector3 (Random.Range (-3f, 3f), Random.Range (-3f, 3f));
         PhotonNetwork.Instantiate ("GamePlayer", v, Quaternion.identity);
     }
