dc71f809796ab40296618d977d90aad07876eb64
Moosan <washer2016@outlook.jp>
2019-11-01 05:21:49 +0900

    DeleteProto

--- tkjp/Assets/TKJP/Scripts/Player/DefenceGrabbable.cs
+++ tkjp/Assets/TKJP/Scripts/Player/DefenceGrabbable.cs
@@ -1,9 +1,11 @@
 ﻿using TKJP.Feature.Attack;
+using TKJP.Feature.Hp;
 using UniRx.Async;
 namespace TKJP.Player
 {
     public class DefenceGrabbable : TKJPGrabbable, IAttackable
     {
+        protected HpAgent HpAgent;
         public virtual async UniTask<AttackResult> Attack(AttackState state)
         {
             await UniTask.CompletedTask;
