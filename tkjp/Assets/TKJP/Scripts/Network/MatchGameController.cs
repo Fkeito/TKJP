4482c3d1dc1495f8ef38fdc9729dc81b4c92576a
HnakadouriN <hasebenaoki0908@docomo.ne.jp>
2019-10-31 22:59:26 +0900

    CreateRoom

---
+++ tkjp/Assets/TKJP/Scripts/Network/MatchGameController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchGameController : MonoBehaviour
+{
+    private RandomRoomMatcher _randomRoomMatcher;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _randomRoomMatcher = gameObject.AddComponent<RandomRoomMatcher>();
+        _randomRoomMatcher.ConnectStart();
+    }
+
+    public void StartMatching()
+    {
+        _randomRoomMatcher.JoinOrCreat();
+    }
+}
