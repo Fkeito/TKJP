1d9cc543d8f45ddfbbb40861ee9bc79b7cfa1a99
Fkeito <kei1229tomi@gmail.com>
2019-11-01 09:28:12 +0900

    Button Ga Osesou

---
+++ tkjp/Assets/TKJP/Scripts/UI/TKJPUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TKJP.UI
+{
+    public abstract class TKJPUI : MonoBehaviour, ITKJPGrab, ITKJPTouch
+    {
+        public virtual void OnTouchBegin() { }
+        public virtual void OnTouch() { }
+        public virtual void OnTouchEnd() { }
+        public virtual void OnGrabBegin() { }
+        public virtual void OnGrab() { }
+        public virtual void OnGrabEnd() { }
+    }
+}
---
+++ tkjp/Assets/TKJP/Scripts/UI/TKJPUIHand.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TKJP.Player;
+
+namespace TKJP.UI
+{
+    public class TKJPUIHand : MonoBehaviour
+    {
+        // Grip trigger thresholds for picking up objects, with some hysteresis.
+        public float grabBegin = 0.55f;
+        public float grabEnd = 0.35f;
+        private float prevFlex;
+        private bool isGrab;
+        private bool prevGrab;
+
+        public TKJPController.HandType handType;
+
+        private void Update()
+        {
+            float _prev = prevFlex;
+            prevFlex = OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger);
+            CheckForGrabOrRelease(_prev);
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            ITKJPTouch touch = other.GetComponent<ITKJPTouch>();
+            touch?.OnTouchBegin();
+            //Todo: HandModel透明化
+        }
+        void OnTriggerStay(Collider other)
+        {
+            ITKJPTouch touch = other.GetComponent<ITKJPTouch>();
+            touch?.OnTouch();
+
+            ITKJPGrab grab = other.GetComponent<ITKJPGrab>();
+            if (prevGrab != isGrab)
+            {
+                if (isGrab) grab?.OnGrabBegin();
+                else grab?.OnGrabEnd();
+            }
+            if (isGrab)
+            {
+                grab?.OnGrab();
+            }
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            ITKJPTouch touch = other.GetComponent<ITKJPTouch>();
+            touch?.OnTouchEnd();
+            isGrab = false;
+            //HandModel透明化解除
+        }
+
+        protected void CheckForGrabOrRelease(float prev)
+        {
+            if ((prevFlex >= grabBegin) && (prev < grabBegin))
+            {
+                isGrab = true;
+            }
+            else if ((prevFlex <= grabEnd) && (prev > grabEnd))
+            {
+                isGrab = false;
+            }
+        }
+    }
+}
