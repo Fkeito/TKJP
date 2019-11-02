a97fbb747b42e413893dedb40d63cdd96ef8ba43
HnakadouriN <hasebenaoki0908@docomo.ne.jp>
2019-11-02 15:37:41 +0900

    hpholder

---
+++ tkjp/Assets/TKJP/Scripts/Feature/Hp/HpHolder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UniRx;
+
+namespace TKJP.Feature.Hp
+{
+    public class HpHolder : IHpHolder
+    {
+        public int Hp {
+            get {
+                return _hp;
+            }
+            set
+            {
+                HpChangeSubject?.OnNext(value);
+                _hp = value;
+            }
+        }
+        private int _hp;
+
+        public IObservable<int> OnChangeHp => HpChangeSubject;
+
+        private readonly BehaviorSubject<int> HpChangeSubject;
+
+        public HpHolder(int hp)
+        {
+            _hp = hp;
+            HpChangeSubject = new BehaviorSubject<int>(hp);
+        }
+    }
+}
