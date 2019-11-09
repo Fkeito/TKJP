45a7d30435a01eb58d338b644364845ac412eadf
Fkeito <kei1229tomi@gmail.com>
2019-11-09 18:20:42 +0900

    jadge battle result

--- tkjp/Assets/TKJP/Scripts/Battle/Game/BattleManager.cs
+++ tkjp/Assets/TKJP/Scripts/Battle/Game/BattleManager.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 using TKJP.Player;
 using TKJP.Battle.State;
+using TKJP.Feature.Hp;
 
 namespace TKJP.Battle.Game
 {
@@ -12,10 +14,16 @@ namespace TKJP.Battle.Game
         private ResultManager result;
         private TKJPGrabber.GrabType grabType;
 
+        public ScarecrowController clientPlayer;
+        //public PlayerController masterPlayer;
+
         void Start()
         {
             state = Manager.GetState<BattleState>();
             result = Manager.GetStateObj(State.State.Result).GetComponent<ResultManager>();
+
+            clientPlayer.GetHp().OnChangeHp.Subscribe(value => { if (value <= 0) BeSettled(Result.Win); }).AddTo(gameObject);
+            //masterPlayer.GetHp().OnChangeHp.Subscribe(value => { if (value <= 0) BeSettled(Result.Lose); }).AddTo(gameObject);
         }
 
         void OnEnable()
