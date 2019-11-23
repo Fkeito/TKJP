44d82c4bd284cd2b1f53269a0502877cefc001d8
Moosan <washer2016@outlook.jp>
2019-11-23 10:52:44 +0900

    test

--- tkjp/Assets/TKJP/Scripts/Battle/Game/BattleManager.cs
+++ tkjp/Assets/TKJP/Scripts/Battle/Game/BattleManager.cs
@@ -45,6 +45,11 @@ namespace TKJP.Battle.Game
 
             //clientPlayer.GetHp().OnChangeHp.Subscribe(value => { if (value <= 0) BeSettled(Result.Win); }).AddTo(gameObject);
             //masterPlayer.GetHp().OnChangeHp.Subscribe(value => { if (value <= 0) BeSettled(Result.Lose); }).AddTo(gameObject);
+            if(PhotonNetwork.CurrentRoom.PlayerCount == 1)
+            {
+                DebugEnemy?.GetHp().OnChangeHp.Subscribe(value => { if (value <= 0) BeSettled(Result.Win); }).AddTo(gameObject);
+            }
+
             MyPlayer = SceneContainer.ContextObj<TKJPPlayer>();
             HpDisposable?.Dispose();
             HpDisposable = MyPlayer
